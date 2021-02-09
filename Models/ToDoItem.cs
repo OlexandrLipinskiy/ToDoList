eddab0f2352a366384b551f9ecab8b77bcf32a27
OlexandrLipinskiy <caia.kot@gmail.com>
2021-02-09 18:35:25 +0200

    first commit

---
+++ Models/ToDoItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ToDoList.Models
+{
+    public class ToDoItem
+    {
+        public int Id { get; set; }
+
+        [StringLength(80)]
+        public string Description { get; set; }
+
+        [Display(Name = "Is Done")]
+        public bool IsDone { get; set; }
+    }
+}
